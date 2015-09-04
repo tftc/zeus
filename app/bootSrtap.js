8bd309328703c8ebd6d68b0771dcb6196c7fba3d
tshawn <371614518@qq.com>
2015-09-04 10:51:52 +0800

    ejs demo
    ejs demo

--- app/bootSrtap.js
+++ app/bootSrtap.js
@@ -1,6 +1,18 @@
 var koa = require('koa');
+var render = require('koa-ejs');
+var path = require('path');
 var app = koa();
-app.use(function * (){
-    this.body = '11111';
-})
+render(app, {
+    root: path.join(__dirname, 'template'),
+    layout: 'layout',
+    viewExt: 'html',
+    cache: false,
+    debug: true
+});
+app.use(function *() {
+    var users = [{name: 'Dead Horse'}, {name: 'Jack'}, {name: 'Tom'}];
+    yield this.render('user', {
+        users : users
+    });
+});
 app.listen(8080);
